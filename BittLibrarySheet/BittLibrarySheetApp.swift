b3b0da82302f1abf03bbdc517666ed678004c0d5
Josh Sorokin <jrsorokin@gmail.com>
2024-06-19 16:14:01 +0300

    Initial Commit

---
+++ BittLibrarySheet/BittLibrarySheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BittLibrarySheetApp.swift
+//  BittLibrarySheet
+//
+//  Created by Emma Sorokin Calderon on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BittLibrarySheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
